82664ee63d0bc2d20e22fb7334e592ccf23e7e58
hotio <hotio@protonmail.com>
2019-12-24 10:24:48 +0100

    [CI SKIP] exit 1 in update.sh

--- update.sh
+++ update.sh
@@ -46,7 +46,7 @@ elif [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
 else
     version=$(curl -fsSL "https://plex.tv/api/downloads/5.json" | jq -r .computer.Linux.version)
-    [[ -z ${version} ]] && exit
+    [[ -z ${version} ]] && exit 1
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG PLEX_VERSION=.*$/ARG PLEX_VERSION=${version}/g" {} \;
     sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
     echo "##[set-output name=version;]${version}"
