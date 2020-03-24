dd4c607384841f88ba777303a2c06ca92d5e371a
hotio <26902309+hotio@users.noreply.github.com>
2020-03-24 10:11:05 +0100

    [CI SKIP]

--- update.sh
+++ update.sh
@@ -56,7 +56,7 @@ elif [[ ${1} == "checkdigests" ]]; then
 else
     version=$(curl -fsSL "https://plex.tv/api/downloads/5.json" | jq -r .computer.Linux.version)
     [[ -z ${version} ]] && exit 1
-    version_plexautoscan=$(curl -fsSL "https://api.github.com/repos/l3uddz/plex_autoscan/commits/master" | jq -r .sha)
+    version_plexautoscan=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/l3uddz/plex_autoscan/commits/master" | jq -r .sha)
     [[ -z ${version_plexautoscan} ]] && exit 1
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG PLEX_VERSION=.*$/ARG PLEX_VERSION=${version}/g" {} \;
     sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
