f8af7bb437a2da8040f41dcb652c4497908d6814
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 08:58:00 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-version=$(curl -fsSL "https://plex.tv/api/downloads/5.json" | jq -re .computer.Linux.version) || exit 1
+set -exuo pipefail
+
+version=$(curl -fsSL "https://plex.tv/api/downloads/5.json" | jq -re .computer.Linux.version)
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
