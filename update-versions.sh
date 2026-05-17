5522bb520e1f027f5cde9c81aa3c0753ad007afe
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 18:01:49 +0200

    add __command [skip ci]

--- update-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-version=$(curl -fsSL "https://plex.tv/api/downloads/5.json" | jq -re .computer.Linux.version)
-json=$(cat meta.json)
-jq --sort-keys \
-    --arg version "${version//v/}" \
-    '.version = $version' <<< "${json}" | tee meta.json
